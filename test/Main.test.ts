cc901f002b746d3b3b5674be3c36a01ffacdfca3
Le Vivilet <levivlet@pm.me>
2025-01-08 18:25:45 +0100

    feature: add initial files

---
+++ test/Main.test.ts
@@ -0,0 +1,7 @@
+/* @jest-environment jsdom */
+import { expect, test } from '@jest/globals'
+import * as Main from '../src/parts/Main/Main.ts'
+
+test('main', () => {
+  expect(typeof Main.main).toBe('function')
+})
