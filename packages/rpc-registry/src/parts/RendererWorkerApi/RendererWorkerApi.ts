ff3ad3445e16b61c9ad32a566a7c63334808ed8c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-03 17:48:31 +0200

    feature: update renderer worker api (#17)

--- packages/rpc-registry/src/parts/RendererWorkerApi/RendererWorkerApi.ts
+++ packages/rpc-registry/src/parts/RendererWorkerApi/RendererWorkerApi.ts
@@ -3,6 +3,7 @@ export interface RendererWorkerApi {
   readonly 'ColorTheme.getColorThemeNames': () => Promise<readonly string[]>
   readonly 'ColorTheme.setColorTheme': (id: string) => Promise<void>
   readonly 'ContextMenu.show': (x: number, y: number, id: any, ...args: readonly any[]) => Promise<void>
+  readonly 'ElectronDialog.showMessageBox': (options: any) => Promise<any>
   readonly 'ErrorHandling.showErrorDialog': (errorInfo: any) => Promise<void>
   readonly 'ExtensionHost.executeCommand': (id: string) => Promise<void>
   readonly 'ExtensionHost.getCommands': () => Promise<readonly any[]>
@@ -12,10 +13,15 @@ export interface RendererWorkerApi {
   readonly 'ExtensionHostManagement.activateByEvent': (event: string) => Promise<void>
   readonly 'FileSystem.readFile': (uri: string) => Promise<string>
   readonly 'Focus.setFocus': (focusKey: number) => Promise<void>
+  readonly 'GetWindowId.getWindowId': () => Promise<number>
   readonly 'IconTheme.getFileIcon': (options: any) => Promise<string>
   readonly 'IconTheme.getFolderIcon': (options: any) => Promise<string>
   readonly 'IconTheme.getIcons': (request: readonly any[]) => Promise<readonly string[]>
   readonly 'Main.openUri': (uri: string, focus?: boolean, props?: any) => Promise<void>
+  readonly 'Process.getChromeVersion': () => Promise<string>
+  readonly 'Process.getElectronVersion': () => Promise<string>
+  readonly 'Process.getNodeVersion': () => Promise<string>
+  readonly 'Process.getV8Version': () => Promise<string>
   readonly 'RecentlyOpened.getRecentlyOpened': () => Promise<readonly string[]>
   readonly 'Run And Debug.handlePaused': (params: any) => Promise<void>
   readonly 'Run And Debug.handleResumed': (params: any) => Promise<void>
@@ -23,7 +29,7 @@ export interface RendererWorkerApi {
   readonly 'SearchProcess.invoke': (command: string, ...args: readonly any[]) => Promise<any>
   readonly 'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker': (port: MessagePort, initialCommand: string, rpcId: number) => Promise<void>
   readonly 'Viewlet.closeWidget': (id: number) => Promise<void>
-  readonly 'Viewlet.openWidget': (id: string) => Promise<void>
+  readonly 'Viewlet.openWidget': (widgetId: string) => Promise<string>
   readonly 'Workspace.getPath': () => Promise<string>
   readonly 'Workspace.setPath': (uri: string) => Promise<void>
 }
