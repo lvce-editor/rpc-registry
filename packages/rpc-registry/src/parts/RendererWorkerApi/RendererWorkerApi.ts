d0afc72832b5db590d4b973697c99318248c40fb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-04 15:18:13 +0200

    feature: add search apis (#20)

--- packages/rpc-registry/src/parts/RendererWorkerApi/RendererWorkerApi.ts
+++ packages/rpc-registry/src/parts/RendererWorkerApi/RendererWorkerApi.ts
@@ -1,7 +1,13 @@
+import type { BulkReplacementEdit } from '../BulkReplacementEdit/BulkReplacementEdit.ts'
+import type { ConfirmPromptOptions } from '../ConfirmPromptOptions/ConfirmPromptOptions.ts'
+import type { SearchResult } from '../SearchResult/SearchResult.ts'
+
 export interface RendererWorkerApi {
+  readonly 'BulkReplacement.applyBulkReplacement': (edits: readonly BulkReplacementEdit[]) => Promise<void>
   readonly 'ClipBoard.writeText': (text: string) => Promise<void>
   readonly 'ColorTheme.getColorThemeNames': () => Promise<readonly string[]>
   readonly 'ColorTheme.setColorTheme': (id: string) => Promise<void>
+  readonly 'ConfirmPrompt.prompt': (confirmText: string, options: ConfirmPromptOptions) => Promise<boolean>
   readonly 'ContextMenu.show': (x: number, y: number, id: any, ...args: readonly any[]) => Promise<void>
   readonly 'ElectronDialog.showMessageBox': (options: any) => Promise<any>
   readonly 'ErrorHandling.showErrorDialog': (errorInfo: any) => Promise<void>
@@ -11,6 +17,10 @@ export interface RendererWorkerApi {
   readonly 'ExtensionHost.searchFileWithHtml': (uri: string) => Promise<readonly string[]>
   readonly 'ExtensionHost.searchFileWithMemory': (uri: string) => Promise<readonly string[]>
   readonly 'ExtensionHostManagement.activateByEvent': (event: string) => Promise<void>
+  readonly 'ExtensionHostTextSearch.executeTextSearchProvider': (scheme: string, query: string) => Promise<readonly SearchResult[]>
+  readonly 'ExtensionHostTextSearch.textSearchFetch': (scheme: string, root: string, query: string, options: any, assetDir: string) => Promise<readonly SearchResult[]>
+  readonly 'ExtensionHostTextSearch.textSearchHtml': (scheme: string, root: string, query: string) => Promise<readonly SearchResult[]>
+  readonly 'ExtensionHostTextSearch.textSearchMemory': (scheme: string, root: string, query: string, options: any, assetDir: string) => Promise<readonly SearchResult[]>
   readonly 'FileSystem.readFile': (uri: string) => Promise<string>
   readonly 'Focus.setFocus': (focusKey: number) => Promise<void>
   readonly 'GetWindowId.getWindowId': () => Promise<number>
@@ -28,6 +38,7 @@ export interface RendererWorkerApi {
   readonly 'Run And Debug.handleResumed': (params: any) => Promise<void>
   readonly 'Run And Debug.handleScriptParsed': (params: any) => Promise<void>
   readonly 'SearchProcess.invoke': (command: string, ...args: readonly any[]) => Promise<any>
+  readonly 'SendMessagePortToElectron.sendMessagePortToElectron': (port: MessagePort, command: string) => Promise<void>
   readonly 'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker': (port: MessagePort, initialCommand: string, rpcId: number) => Promise<void>
   readonly 'Viewlet.closeWidget': (id: number | string) => Promise<void>
   readonly 'Viewlet.openWidget': (widgetId: string) => Promise<string>
