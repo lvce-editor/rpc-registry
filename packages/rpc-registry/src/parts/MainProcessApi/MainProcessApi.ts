0c53294df4a46879063c47abfd1d3f63e4f006c4
Le Vivilet <levivlet@pm.me>
2025-05-08 22:23:08 +0200

    feature: expand main process api

--- packages/rpc-registry/src/parts/MainProcessApi/MainProcessApi.ts
+++ packages/rpc-registry/src/parts/MainProcessApi/MainProcessApi.ts
@@ -1,3 +1,14 @@
 export interface MainProcessApi {
+  readonly 'ElectronWebContents.callFunction': (webContentsId: any, method: string, ...params: readonly any[]) => Promise<any>
+  readonly 'ElectronWebContents.dispose': (id: any) => Promise<void>
   readonly 'ElectronWebContentsView.attachEventListeners': (webContentsId: any) => Promise<void>
+  readonly 'ElectronWebContentsView.createWebContentsView': (webContentsId: any) => Promise<any>
+  readonly 'ElectronWebContentsView.disposeWebContentsView': (webContentsId: any) => Promise<void>
+  readonly 'ElectronWebContentsViewFunctions.getStats': (id: any, ...args: readonly any[]) => Promise<any>
+  readonly 'ElectronWebContentsViewFunctions.resizeBrowserView': (id: any, ...args: readonly any[]) => Promise<void>
+  readonly 'ElectronWebContentsViewFunctions.setBackgroundColor': (webContentsId: any, backgroundColor: string) => Promise<void>
+  readonly 'ElectronWebContentsViewFunctions.setFallthroughKeyBindings': (id: any, ...args: readonly any[]) => Promise<void>
+  readonly 'ElectronWebContentsViewFunctions.setIframeSrc': (id: any, ...args: readonly any[]) => Promise<void>
+  readonly 'ElectronWebContentsViewFunctions.setIframeSrcFallback': (id: any, ...args: readonly any[]) => Promise<void>
+  readonly 'ElectronWebContentsViewFunctions.show': (id: any, ...args: readonly any[]) => Promise<void>
 }
