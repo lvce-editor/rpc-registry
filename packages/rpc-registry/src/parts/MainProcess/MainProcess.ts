d16d3468a805ef0c1c79a506525db5285c5de68c
Le Vivilet <levivlet@pm.me>
2025-05-08 22:18:27 +0200

    feature: add main process api

---
+++ packages/rpc-registry/src/parts/MainProcess/MainProcess.ts
@@ -0,0 +1,20 @@
+import type { Rpc } from '@lvce-editor/rpc'
+import type { MainProcessApi } from '../MainProcessApi/MainProcessApi.ts'
+import * as RpcId from '../RpcId/RpcId.ts'
+import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
+
+export const invoke = <T extends keyof MainProcessApi>(method: T, ...params: Parameters<MainProcessApi[T]>): ReturnType<MainProcessApi[T]> => {
+  const rpc = RpcRegistry.get(RpcId.RendererWorker)
+  // @ts-ignore
+  return rpc.invoke(method, ...params)
+}
+
+export const invokeAndTransfer = <T extends keyof MainProcessApi>(method: T, ...params: Parameters<MainProcessApi[T]>): ReturnType<MainProcessApi[T]> => {
+  const rpc = RpcRegistry.get(RpcId.RendererWorker)
+  // @ts-ignore
+  return rpc.invokeAndTransfer(method, ...params)
+}
+
+export const set = (rpc: Rpc): void => {
+  RpcRegistry.set(RpcId.MainProcess, rpc)
+}
