557cf658acd779a0e3a4b03f01f134660e482c7e
Le Vivilet <levivlet@pm.me>
2025-05-17 22:34:42 +0200

    fix

--- packages/rpc-registry/src/parts/MainProcess/MainProcess.ts
+++ packages/rpc-registry/src/parts/MainProcess/MainProcess.ts
@@ -2,4 +2,4 @@ import type { MainProcessApi } from '../MainProcessApi/MainProcessApi.ts'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
-export const { invoke, invokeAndTransfer, set } = RpcFactory.create<MainProcessApi>(RpcId.SharedProcess)
+export const { invoke, invokeAndTransfer, set } = RpcFactory.create<MainProcessApi>(RpcId.MainProcess)
