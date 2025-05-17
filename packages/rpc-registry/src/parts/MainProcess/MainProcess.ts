a777a117ac1daa7875d07a8e566c43da7a9d0bd0
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-17 22:35:47 +0200

    feature: add editor worker api (#37)
    * feature: add editor worker api
    
    * export
    
    * fix
    
    * fix

--- packages/rpc-registry/src/parts/MainProcess/MainProcess.ts
+++ packages/rpc-registry/src/parts/MainProcess/MainProcess.ts
@@ -1,20 +1,5 @@
-import type { Rpc } from '@lvce-editor/rpc'
 import type { MainProcessApi } from '../MainProcessApi/MainProcessApi.ts'
+import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
-import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
-export const invoke = <T extends keyof MainProcessApi>(method: T, ...params: Parameters<MainProcessApi[T]>): ReturnType<MainProcessApi[T]> => {
-  const rpc = RpcRegistry.get(RpcId.MainProcess)
-  // @ts-ignore
-  return rpc.invoke(method, ...params)
-}
-
-export const invokeAndTransfer = <T extends keyof MainProcessApi>(method: T, ...params: Parameters<MainProcessApi[T]>): ReturnType<MainProcessApi[T]> => {
-  const rpc = RpcRegistry.get(RpcId.MainProcess)
-  // @ts-ignore
-  return rpc.invokeAndTransfer(method, ...params)
-}
-
-export const set = (rpc: Rpc): void => {
-  RpcRegistry.set(RpcId.MainProcess, rpc)
-}
+export const { invoke, invokeAndTransfer, set } = RpcFactory.create<MainProcessApi>(RpcId.MainProcess)
--- packages/rpc-registry/src/parts/SharedProcess/SharedProcess.ts
+++ packages/rpc-registry/src/parts/SharedProcess/SharedProcess.ts
@@ -1,20 +1,5 @@
-import type { Rpc } from '@lvce-editor/rpc'
 import type { SharedProcessApi } from '../SharedProcessApi/SharedProcessApi.ts'
+import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
-import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
-export const invoke = <T extends keyof SharedProcessApi>(method: T, ...params: Parameters<SharedProcessApi[T]>): ReturnType<SharedProcessApi[T]> => {
-  const rpc = RpcRegistry.get(RpcId.SharedProcess)
-  // @ts-ignore
-  return rpc.invoke(method, ...params)
-}
-
-export const invokeAndTransfer = <T extends keyof SharedProcessApi>(method: T, ...params: Parameters<SharedProcessApi[T]>): ReturnType<SharedProcessApi[T]> => {
-  const rpc = RpcRegistry.get(RpcId.SharedProcess)
-  // @ts-ignore
-  return rpc.invokeAndTransfer(method, ...params)
-}
-
-export const set = (rpc: Rpc): void => {
-  RpcRegistry.set(RpcId.SharedProcess, rpc)
-}
+export const { invoke, invokeAndTransfer, set } = RpcFactory.create<SharedProcessApi>(RpcId.SharedProcess)
