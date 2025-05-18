7925008e397d0a72bc1fe06fee3b52069a1c28d2
Le Vivilet <levivlet@pm.me>
2025-05-18 13:27:28 +0200

    fix: rpc id

--- packages/rpc-registry/src/parts/RpcFactory/RpcFactory.ts
+++ packages/rpc-registry/src/parts/RpcFactory/RpcFactory.ts
@@ -1,19 +1,18 @@
 /* eslint-disable  @typescript-eslint/explicit-function-return-type */
 import type { RpcFactoryResult } from '../RpcFactoryResult/RpcFactoryResult.ts'
-import * as RpcId from '../RpcId/RpcId.ts'
 import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
 export const create = <Api extends Record<string, any>>(rpcId: number): RpcFactoryResult<Api> => {
   return {
     // @ts-ignore
     invoke(method, ...params) {
-      const rpc = RpcRegistry.get(RpcId.RendererWorker)
+      const rpc = RpcRegistry.get(rpcId)
       // @ts-ignore
       return rpc.invoke(method, ...params)
     },
     // @ts-ignore
     invokeAndTransfer(method, ...params) {
-      const rpc = RpcRegistry.get(RpcId.RendererWorker)
+      const rpc = RpcRegistry.get(rpcId)
       // @ts-ignore
       return rpc.invokeAndTransfer(method, ...params)
     },
