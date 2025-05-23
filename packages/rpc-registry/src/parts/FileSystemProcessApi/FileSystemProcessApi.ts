c15b5b9c9b56a7c6826cd5a079f2bb5eeea67a3d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-23 22:52:35 +0200

    feature: update file system process api (#51)

--- packages/rpc-registry/src/parts/FileSystemProcessApi/FileSystemProcessApi.ts
+++ packages/rpc-registry/src/parts/FileSystemProcessApi/FileSystemProcessApi.ts
@@ -1,3 +1,12 @@
 export interface FileProcessApi {
+  readonly 'FileSystem.copy': (oldUri: string, newUri: string) => Promise<void>
+  readonly 'FileSystem.getPathSeparator': () => Promise<string>
+  readonly 'FileSystem.getRealPath': (uri: string) => Promise<string>
+  readonly 'FileSystem.mkdir': (uri: string) => Promise<void>
+  readonly 'FileSystem.readDirWithFileTypes': (uri: string) => Promise<readonly any[]>
   readonly 'FileSystem.readFile': (uri: string) => Promise<string>
+  readonly 'FileSystem.remove': (uri: string) => Promise<void>
+  readonly 'FileSystem.rename': (oldUri: string, newUri: string) => Promise<void>
+  readonly 'FileSystem.stat': (uri: string) => Promise<any>
+  readonly 'FileSystem.writeFile': (uri: string, content: string) => Promise<void>
 }
