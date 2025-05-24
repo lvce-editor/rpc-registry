624c5eebb55390c90a95fde12ed31f70e337d1a3
Le Vivilet <levivlet@pm.me>
2025-05-24 09:02:28 +0200

    feature: add file system worker api

--- packages/rpc-registry/src/parts/FileSystemProcess/FileSystemProcess.ts
+++ packages/rpc-registry/src/parts/FileSystemProcess/FileSystemProcess.ts
@@ -1,5 +1,5 @@
-import type { FileProcessApi } from '../FileSystemProcessApi/FileSystemProcessApi.ts'
+import type { FileSystemProcessApi } from '../FileSystemProcessApi/FileSystemProcessApi.ts'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
-export const { invoke, invokeAndTransfer, set } = RpcFactory.create<FileProcessApi>(RpcId.FileSystemProcess)
+export const { invoke, invokeAndTransfer, set } = RpcFactory.create<FileSystemProcessApi>(RpcId.FileSystemProcess)
