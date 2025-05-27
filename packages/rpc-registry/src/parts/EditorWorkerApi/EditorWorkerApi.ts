e1129099dfe629cecee7a711c821b19e8c3ea504
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-27 19:45:02 +0200

    feature: add getWordAtOffset api (#70)

--- packages/rpc-registry/src/parts/EditorWorkerApi/EditorWorkerApi.ts
+++ packages/rpc-registry/src/parts/EditorWorkerApi/EditorWorkerApi.ts
@@ -3,4 +3,5 @@ import type { PositionAtCursor } from '../PositionAtCursor/PositionAtCursor.ts'
 export interface EditorWorkerApi {
   readonly 'Editor.getPositionAtCursor': (uid: number) => Promise<PositionAtCursor>
   readonly 'Editor.getWordAt2': (uid: number, rowIndex: number, columnIndex: number) => Promise<string>
+  readonly 'Editor.getWordAtOffset2': (uid: number) => Promise<string>
 }
