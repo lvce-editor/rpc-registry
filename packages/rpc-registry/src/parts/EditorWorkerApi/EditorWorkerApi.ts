f857e2ce4177a2c4670f78afa3c4749160cb5886
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-29 21:52:59 +0200

    feature: add editor selection api (#73)

--- packages/rpc-registry/src/parts/EditorWorkerApi/EditorWorkerApi.ts
+++ packages/rpc-registry/src/parts/EditorWorkerApi/EditorWorkerApi.ts
@@ -5,4 +5,6 @@ export interface EditorWorkerApi {
   readonly 'Editor.getWordAt2': (uid: number, rowIndex: number, columnIndex: number) => Promise<string>
   readonly 'Editor.getWordAtOffset2': (uid: number) => Promise<string>
   readonly 'Editor.getWordBefore2': (editorUid: number, rowIndex: number, columnIndex: number) => Promise<string>
+  readonly 'Editor.getLines2': (editorUid: number) => Promise<readonly string[]>
+  readonly 'Editor.getSelections2': (editorUid: number) => Promise<readonly number[]>
 }
