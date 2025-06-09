d049521c8be8ef08600de9763ad1bf77d5bb554c
Le Vivilet <levivlet@pm.me>
2025-06-09 11:22:10 +0200

    feature: add more exact syntax highlighting worker api

--- packages/rpc-registry/src/parts/SyntaxHighlightingWorkerApi/SyntaxHighlightingWorkerApi.ts
+++ packages/rpc-registry/src/parts/SyntaxHighlightingWorkerApi/SyntaxHighlightingWorkerApi.ts
@@ -1,3 +1,15 @@
 export interface SyntaxHighlightingWorkerApi {
-  readonly 'Syntax.highlight': (lines: readonly string[]) => Promise<readonly any[]>
+  readonly 'GetTokensViewport.getTokensViewport': (
+    editor: any,
+    startLineIndex: number,
+    endLineIndex: number,
+    hasLinesToSend: boolean,
+    id: number,
+    linesToSend: readonly any[],
+  ) => Promise<any>
+  readonly 'HandleMessagePort.handleMessagePort': (port: MessagePort) => Promise<void>
+  readonly 'TextDocument.setLines': (documentId: string, lines: readonly string[]) => Promise<void>
+  readonly 'Tokenizer.load': (languageId: string, tokenizePath: string) => Promise<void>
+  readonly 'Tokenizer.tokenizeCodeBlock': (codeBlock: string, languageId: string, tokenizerPath: string) => Promise<any>
+  readonly 'Tokenizer.tokenizeIncremental': (id: any, languageId: string, oldLine: string, newLine: string, rowIndex: number, minLineY: number) => any[] | undefined
 }
