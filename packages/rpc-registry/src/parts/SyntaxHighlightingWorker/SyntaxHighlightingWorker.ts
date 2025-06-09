64380ddc3b96a205fc3fec00f8671c3f94374a50
Le Vivilet <levivlet@pm.me>
2025-06-09 11:08:19 +0200

    iint

--- packages/rpc-registry/src/parts/SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts
+++ packages/rpc-registry/src/parts/SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts
@@ -1,5 +1,5 @@
+import type { SyntaxHighlightingWorkerApi } from '../SyntaxHighlightingWorkerApi/SyntaxHighlightingWorkerApi.ts'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
-import { SyntaxHighlightingWorkerApi } from '../SyntaxHighlightingWorkerApi/SyntaxHighlightingWorkerApi.ts'
 
 export const { invoke, invokeAndTransfer, set } = RpcFactory.create<SyntaxHighlightingWorkerApi>(RpcId.MarkdownWorker)
