5279798fbdc8fc81e368ffd096b7f9bcf0066469
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-10 19:16:20 +0200

    feature: add updatedebugInfo command (#81)

--- packages/rpc-registry/src/parts/EditorWorkerApi/EditorWorkerApi.ts
+++ packages/rpc-registry/src/parts/EditorWorkerApi/EditorWorkerApi.ts
@@ -7,4 +7,5 @@ export interface EditorWorkerApi {
   readonly 'Editor.getWordBefore2': (editorUid: number, rowIndex: number, columnIndex: number) => Promise<string>
   readonly 'Editor.getLines2': (editorUid: number) => Promise<readonly string[]>
   readonly 'Editor.getSelections2': (editorUid: number) => Promise<readonly number[]>
+  readonly 'Editor.updateDebugInfo': (key: string) => Promise<void>
 }
