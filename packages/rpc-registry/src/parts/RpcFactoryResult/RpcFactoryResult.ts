ed3adb52ff0fabd9ba162580fb8e64f4a27466e8
Le Vivilet <levivlet@pm.me>
2025-06-13 22:35:17 +0200

    feature: add rpc dispose function

--- packages/rpc-registry/src/parts/RpcFactoryResult/RpcFactoryResult.ts
+++ packages/rpc-registry/src/parts/RpcFactoryResult/RpcFactoryResult.ts
@@ -4,4 +4,5 @@ export interface RpcFactoryResult<Api extends Record<string, any>> {
   readonly invoke: <T extends keyof Api>(method: T, ...params: Parameters<Api[T]>) => ReturnType<Api[T]>
   readonly invokeAndTransfer: <T extends keyof Api>(method: T, ...params: Parameters<Api[T]>) => ReturnType<Api[T]>
   readonly set: (rpc: Rpc) => void
+  readonly dispose: () => Promise<void>
 }
