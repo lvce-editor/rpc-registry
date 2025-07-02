e9b2d0c43ea885496239ec703e778db9cf7e64fa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 17:01:07 +0200

    feature: add clipboard read api (#100)
    * feature: add clipboard read api
    
    * update api
    
    * test

--- packages/rpc-registry/src/parts/RendererProcess/RendererProcess.ts
+++ packages/rpc-registry/src/parts/RendererProcess/RendererProcess.ts
@@ -3,3 +3,13 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
 export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<RendererProcessApi>(RpcId.RendererProcess)
+
+export const readClipBoardText = async (): Promise<any> => {
+  // @ts-ignore
+  return await RendererProcess.invoke('ClipBoard.readText')
+}
+
+export const readClipBoard = async (): Promise<any> => {
+  // @ts-ignore
+  return await RendererProcess.invoke('ClipBoard.read')
+}
