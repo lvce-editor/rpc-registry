96aed71a0f1cc7eafb1def7f423e9800fc0a328d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 17:03:45 +0200

    feature: add file system worker api (#101)

--- packages/rpc-registry/src/parts/FileSystemWorker/FileSystemWorker.ts
+++ packages/rpc-registry/src/parts/FileSystemWorker/FileSystemWorker.ts
@@ -3,3 +3,44 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
 export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<FileSystemWorkerApi>(RpcId.FileSystemWorker)
+
+export const remove = async (dirent: string): Promise<void> => {
+  return invoke('FileSystem.remove', dirent)
+}
+
+export const readDirWithFileTypes = async (uri: string): Promise<readonly any[]> => {
+  return invoke('FileSystem.readDirWithFileTypes', uri)
+}
+
+export const getPathSeparator = async (root: string): Promise<string> => {
+  // @ts-ignore
+  return invoke('FileSystem.getPathSeparator', root)
+}
+
+export const getRealPath = async (path: string): Promise<string> => {
+  return invoke('FileSystem.getRealPath', path)
+}
+
+export const stat = async (dirent: string): Promise<any> => {
+  return invoke('FileSystem.stat', dirent)
+}
+
+export const createFile = async (uri: string): Promise<void> => {
+  return invoke('FileSystem.writeFile', uri, '')
+}
+
+export const writeFile = async (uri: string, content: string): Promise<void> => {
+  return invoke('FileSystem.writeFile', uri, content)
+}
+
+export const mkdir = async (uri: string): Promise<void> => {
+  return invoke('FileSystem.mkdir', uri)
+}
+
+export const rename = async (oldUri: string, newUri: string): Promise<void> => {
+  return invoke('FileSystem.rename', oldUri, newUri)
+}
+
+export const copy = async (oldUri: string, newUri: string): Promise<void> => {
+  return invoke('FileSystem.copy', oldUri, newUri)
+}
