39b6cec4bfedffad85b792b218d016710c1b58e1
Le Vivilet <levivlet@pm.me>
2025-07-02 16:21:25 +0200

    feature: add api functions simplifying api usage

--- packages/rpc-registry/src/parts/RendererWorker/RendererWorker.ts
+++ packages/rpc-registry/src/parts/RendererWorker/RendererWorker.ts
@@ -3,3 +3,22 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
 export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<RendererWorkerApi>(RpcId.RendererWorker)
+
+export const sendMessagePortToEditorWorker = async (port: MessagePort): Promise<void> => {
+  const command = 'HandleMessagePort.handleMessagePort'
+  // @ts-ignore
+  await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker', port, command, RpcId.DebugWorker)
+}
+
+export const readFile = async (uri: string): Promise<string> => {
+  return invoke('FileSystem.readFile', uri)
+}
+
+export const sendMessagePortToExtensionHostWorker = async (port: MessagePort): Promise<void> => {
+  const command = 'HandleMessagePort.handleMessagePort2'
+  await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker', port, command, RpcId.DebugWorker)
+}
+
+export const activateByEvent = (event: string): Promise<void> => {
+  return invoke('ExtensionHostManagement.activateByEvent', event)
+}
