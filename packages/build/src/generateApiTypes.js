23e7c5d132b4921f5d3142c4f2e02477971abfa2
Le Vivilet <levivlet@pm.me>
2025-07-02 17:02:51 +0200

    feature: add format command

--- packages/build/src/generateApiTypes.js
+++ packages/build/src/generateApiTypes.js
@@ -11,26 +11,12 @@ const getActualContent = (content) => {
 
 export const generateApiTypes = async () => {
   const ext = process.platform === 'win32' ? '' : ''
-  const bundleGeneratorPath = join(
-    root,
-    'packages',
-    'build',
-    'node_modules',
-    '.bin',
-    'dts-bundle-generator' + ext,
-  )
-  await execa(
-    bundleGeneratorPath,
-    ['-o', '../../.tmp/dist/dist/index.d.ts', 'src/parts/Main/Main.ts'],
-    {
-      cwd: join(root, 'packages', 'rpc-registry'),
-      reject: false,
-    },
-  )
-  const content = await readFile(
-    join(root, '.tmp', 'dist', 'dist', 'index.d.ts'),
-    'utf8',
-  )
+  const bundleGeneratorPath = join(root, 'packages', 'build', 'node_modules', '.bin', 'dts-bundle-generator' + ext)
+  await execa(bundleGeneratorPath, ['-o', '../../.tmp/dist/dist/index.d.ts', 'src/parts/Main/Main.ts'], {
+    cwd: join(root, 'packages', 'rpc-registry'),
+    reject: false,
+  })
+  const content = await readFile(join(root, '.tmp', 'dist', 'dist', 'index.d.ts'), 'utf8')
   const actual = getActualContent(content)
   await writeFile(join(root, '.tmp', 'dist', 'dist', 'index.d.ts'), actual)
 }
