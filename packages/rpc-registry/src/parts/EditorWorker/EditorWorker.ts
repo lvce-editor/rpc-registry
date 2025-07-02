24f73c66fd4828467bb70166db56c5b750403c47
Le Vivilet <levivlet@pm.me>
2025-07-02 18:02:52 +0200

    feature: add more editor commands

--- packages/rpc-registry/src/parts/EditorWorker/EditorWorker.ts
+++ packages/rpc-registry/src/parts/EditorWorker/EditorWorker.ts
@@ -1,4 +1,6 @@
+import type { Change } from '../Change/Change.ts'
 import type { EditorWorkerApi } from '../EditorWorkerApi/EditorWorkerApi.ts'
+import type { PositionAtCursor } from '../PositionAtCursor/PositionAtCursor.ts'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
@@ -13,4 +15,49 @@ export const sendMessagePortToExtensionHostWorker = async (port: MessagePort): P
     command,
     0, // TODO
   )
+}
+
+// TODO add tests for this
+export const activateByEvent = async (event: string): Promise<void> => {
+  // @ts-ignore
+  await invoke('ActivateByEvent.activateByEvent', event)
+}
+
+export const applyEdit = async (editorUid: number, changes: readonly Change[]): Promise<void> => {
+  // @ts-ignore
+  await EditorWorker.invoke('Editor.applyEdit2', editorUid, changes)
+}
+
+export const closeWidget = async (editorUid: number, widgetId: number, widgetName: string, focusId: number): Promise<void> => {
+  // @ts-ignore
+  await invoke('Editor.closeWidget2', editorUid, widgetId, widgetName, focusId)
+}
+
+export const getWordAt = async (uid: number, rowIndex: number, columnIndex: number): Promise<string> => {
+  // @ts-ignore
+  const word = await invoke('Editor.getWordAt2', uid, rowIndex, columnIndex)
+  return word
+}
+
+export const getLines = async (editorUid: number): Promise<readonly string[]> => {
+  const lines = await invoke('Editor.getLines2', editorUid)
+  return lines
+}
+
+export const getPositionAtCursor = async (parentUid: number): Promise<PositionAtCursor> => {
+  const position = await invoke('Editor.getPositionAtCursor', parentUid)
+  return position
+}
+
+export const getSelections = async (editorUid: number): Promise<readonly number[]> => {
+  const selections = await invoke('Editor.getSelections2', editorUid)
+  return selections
+}
+
+export const getWordAtOffset2 = async (editorUid: number): Promise<string> => {
+  return invoke('Editor.getWordAtOffset2', editorUid)
+}
+
+export const getWordBefore = async (editorUid: number, rowIndex: number, columnIndex: number): Promise<string> => {
+  return invoke('Editor.getWordBefore2', editorUid, rowIndex, columnIndex)
 }
