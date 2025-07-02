72adddfa0ee8dbbb1ec66c5c42944cae0afc79ea
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-02 18:00:37 +0200

    feature: add sendporttoextensionHostWorker (#112)

--- packages/rpc-registry/src/parts/EditorWorker/EditorWorker.ts
+++ packages/rpc-registry/src/parts/EditorWorker/EditorWorker.ts
@@ -3,3 +3,14 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
 export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<EditorWorkerApi>(RpcId.EditorWorker)
+
+export const sendMessagePortToExtensionHostWorker = async (port: MessagePort): Promise<void> => {
+  const command = 'HandleMessagePort.handleMessagePort2'
+  await invokeAndTransfer(
+    // @ts-ignore
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker',
+    port,
+    command,
+    0, // TODO
+  )
+}
