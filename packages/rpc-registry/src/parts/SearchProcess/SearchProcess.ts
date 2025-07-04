eeed16e2d1ca304e024e9c9fa109d1174e2f1d2f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 12:23:21 +0200

    feature: add search api (#154)

--- packages/rpc-registry/src/parts/SearchProcess/SearchProcess.ts
+++ packages/rpc-registry/src/parts/SearchProcess/SearchProcess.ts
@@ -3,3 +3,19 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
 export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<SearchProcessApi>(RpcId.SearchProcess)
+
+// TODO add tests for this
+export const search = async (options: any): Promise<any> => {
+  const result = await invoke('TextSearch.search', options)
+  return result
+}
+
+export const searchIncremental = async (options: any): Promise<any> => {
+  // @ts-ignore
+  return invoke('TextSearch.searchIncremental', options)
+}
+
+export const getIncrementalResults = async (searchId: string, minLineY: number, maxLineY: number): Promise<any> => {
+  // @ts-ignore
+  return invoke('TextSearch.getIncrementalResults', searchId, minLineY, maxLineY)
+}
