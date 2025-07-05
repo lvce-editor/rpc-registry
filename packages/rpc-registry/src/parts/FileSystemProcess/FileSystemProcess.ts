e8820281b6b756e5d22ef4263ac5d5a9442b2bbb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-05 21:03:43 +0200

    feature: add filesystem methods (#163)

--- packages/rpc-registry/src/parts/FileSystemProcess/FileSystemProcess.ts
+++ packages/rpc-registry/src/parts/FileSystemProcess/FileSystemProcess.ts
@@ -3,3 +3,55 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
 export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<FileSystemProcessApi>(RpcId.FileSystemProcess)
+
+export const remove = async (uri: string): Promise<void> => {
+  return invoke('FileSystem.remove', uri)
+}
+
+export const readFile = async (uri: string): Promise<string> => {
+  return invoke('FileSystem.readFile', uri)
+}
+
+export const readDirWithFileTypes = async (uri: string): Promise<readonly any[]> => {
+  return invoke('FileSystem.readDirWithFileTypes', uri)
+}
+
+export const getPathSeparator = async (root: string): Promise<string> => {
+  // @ts-ignore
+  return invoke('FileSystem.getPathSeparator', root)
+}
+
+export const readJson = async (root: string): Promise<any> => {
+  // @ts-ignore
+  return invoke('FileSystem.readJson', root)
+}
+
+export const getRealPath = async (path: string): Promise<string> => {
+  // @ts-ignore
+  return invoke('FileSystem.getRealPath', path)
+}
+
+export const stat = async (path: string): Promise<any> => {
+  // @ts-ignore
+  return invoke('FileSystem.stat', path)
+}
+
+export const writeFile = async (path: string, content: string): Promise<void> => {
+  // @ts-ignore
+  return invoke('FileSystem.writeFile', path, content)
+}
+
+export const mkdir = async (path: string): Promise<void> => {
+  // @ts-ignore
+  return invoke('FileSystem.mkdir', path)
+}
+
+export const rename = async (oldUri: string, newUri: string): Promise<void> => {
+  // @ts-ignore
+  return invoke('FileSystem.rename', oldUri, newUri)
+}
+
+export const copy = async (oldUri: string, newUri: string): Promise<void> => {
+  // @ts-ignore
+  return invoke('FileSystem.copy', oldUri, newUri)
+}
