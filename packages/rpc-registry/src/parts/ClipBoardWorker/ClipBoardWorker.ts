5963b4fec7f1dc87c728eac251ae73dd62b5e035
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-14 11:05:35 +0200

    feature: add clipboard api (#174)

--- packages/rpc-registry/src/parts/ClipBoardWorker/ClipBoardWorker.ts
+++ packages/rpc-registry/src/parts/ClipBoardWorker/ClipBoardWorker.ts
@@ -3,3 +3,23 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 import * as RpcId from '../RpcId/RpcId.ts'
 
 export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<ClipBoardWorkerApi>(RpcId.ClipBoardWorker)
+
+export const writeText = async (text: string): Promise<void> => {
+  // @ts-ignore
+  return invoke('ClipBoard.writeText', text)
+}
+
+export const readText = async (): Promise<string> => {
+  // @ts-ignore
+  return invoke('ClipBoard.readText')
+}
+
+export const writeImage = async (image: any): Promise<void> => {
+  // @ts-ignore
+  return invoke('ClipBoard.writeImage', image)
+}
+
+export const readNativeFiles = async (): Promise<readonly any[]> => {
+  // @ts-ignore
+  return invoke('ClipBoard.readNativeFiles')
+}
