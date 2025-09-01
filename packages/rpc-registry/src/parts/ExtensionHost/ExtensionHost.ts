4585a76348830b56edb45681d9b4f83383d15376
Le Vivilet <levivlet@pm.me>
2025-09-01 21:37:14 +0200

    feature: add extension host mock rpc

--- packages/rpc-registry/src/parts/ExtensionHost/ExtensionHost.ts
+++ packages/rpc-registry/src/parts/ExtensionHost/ExtensionHost.ts
@@ -2,6 +2,7 @@ import { RpcId } from '@lvce-editor/constants'
 import type { ExtensionHostWorkerApi } from '../ExtensionHostWorkerApi/ExtensionHostWorkerApi.ts'
 import type { RuntimeStatus } from '../RuntimeStatus/RuntimeStatus.ts'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
+import { createMockRpc, MockRpc } from '@lvce-editor/rpc'
 
 export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<ExtensionHostWorkerApi>(RpcId.ExtensionHostWorker)
 
@@ -18,4 +19,10 @@ export const executeFileReferenceProvider = async (id: number): Promise<readonly
 export const getRuntimeStatus = async (extensionId: string): Promise<RuntimeStatus> => {
   // @ts-ignore
   return invoke('ExtensionHost.getRuntimeStatus', extensionId)
+}
+
+export const registerMockRpc = (commandMap: Record<string, any>): MockRpc => {
+  const mockRpc = createMockRpc({ commandMap })
+  set(mockRpc)
+  return mockRpc
 }
