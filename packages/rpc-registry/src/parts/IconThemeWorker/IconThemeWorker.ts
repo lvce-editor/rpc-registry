9939fbc8e77d9ecfb6a9e45147610aaf689a3d90
Le Vivilet <levivilet@pm.me>
2025-09-24 23:29:32 +0200

    tsc

--- packages/rpc-registry/src/parts/IconThemeWorker/IconThemeWorker.ts
+++ packages/rpc-registry/src/parts/IconThemeWorker/IconThemeWorker.ts
@@ -1,9 +1,9 @@
 import type { MockRpc } from '@lvce-editor/rpc'
+import { RpcId } from '@lvce-editor/constants'
 import { createMockRpc } from '@lvce-editor/rpc'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
-import { RpcId } from '@lvce-editor/constants'
 
-export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<{}>(RpcId.IconThemeWorker)
+export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create<object>(RpcId.IconThemeWorker)
 
 export const getIcons = async (iconRequests: readonly any[]): Promise<readonly any[]> => {
   // @ts-ignore
