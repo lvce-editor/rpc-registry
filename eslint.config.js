0d81f5736c27e5bd4d3ffd6dc5ae8594cf8286cc
Le Vivilet <levivilet@pm.me>
2025-11-20 17:06:46 +0100

    feature: create prerelease before release (#289)

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import * as config from '@lvce-editor/eslint-config'
+import * as actions from '@lvce-editor/eslint-plugin-github-actions'
 
 export default [
   ...config.default,
   ...config.recommendedNode,
+  ...actions.default,
   {
     files: ['**/*.ts'],
     rules: {
@@ -14,6 +16,11 @@ export default [
     files: ['**/package.json'],
     rules: {
       'package-json/require-description': 'off',
+    },
+  },
+  {
+    rules: {
+      'github-actions/ci-versions': 'off',
     },
   },
 ]
