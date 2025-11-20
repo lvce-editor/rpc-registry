0eb5aab9ccb0974b07a52f1eeeb69811808a3976
Le Vivilet <levivilet@pm.me>
2025-11-20 16:56:18 +0100

    feature: add text measurement worker (#285)

---
+++ packages/rpc-registry/src/parts/TextMeasurementWorker/TextMeasurementWorker.ts
@@ -0,0 +1,12 @@
+import type { MockRpc } from '@lvce-editor/rpc'
+import { RpcId } from '@lvce-editor/constants'
+import { createMockRpc } from '@lvce-editor/rpc'
+import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
+
+export const { invoke, invokeAndTransfer, set, dispose } = RpcFactory.create(RpcId.IconThemeWorker)
+
+export const registerMockRpc = (commandMap: Record<string, any>): MockRpc => {
+  const mockRpc = createMockRpc({ commandMap })
+  set(mockRpc)
+  return mockRpc
+}
