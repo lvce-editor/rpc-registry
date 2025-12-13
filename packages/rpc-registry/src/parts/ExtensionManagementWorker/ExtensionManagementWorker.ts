e75d8d82a1de447d2dce5f8ab741e0f628dc00cf
Le Vivilet <levivilet@pm.me>
2025-12-13 22:43:46 +0100

    feature: add extension management worker (#308)
    * feature: add extension management worker
    
    * fix
    
    * add commands
    
    * add fn

---
+++ packages/rpc-registry/src/parts/ExtensionManagementWorker/ExtensionManagementWorker.ts
@@ -0,0 +1,33 @@
+import type { MockRpc } from '@lvce-editor/rpc'
+import { createMockRpc } from '@lvce-editor/rpc'
+import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
+
+const id = 9006
+
+export const { dispose, invoke, invokeAndTransfer, set } = RpcFactory.create(id)
+
+export const registerMockRpc = (commandMap: Record<string, any>): MockRpc => {
+  const mockRpc = createMockRpc({ commandMap })
+  set(mockRpc)
+  return mockRpc
+}
+
+export const enable = (id: string): Promise<void> => {
+  return invoke(`Extensions.enable`, id)
+}
+
+export const disable = (id: string): Promise<void> => {
+  return invoke(`Extensions.disable`, id)
+}
+
+export const getExtension = (id: string): Promise<any> => {
+  return invoke(`Extensions.getExtension`, id)
+}
+
+export const install = (id: string): Promise<void> => {
+  return invoke(`Extensions.install`, id)
+}
+
+export const uninstall = (id: string): Promise<void> => {
+  return invoke(`Extensions.uninstall`, id)
+}
