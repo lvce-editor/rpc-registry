391d8e349604ccdefbfdbc1f4d3b8a6a54dc19c4
Le Vivilet <levivilet@pm.me>
2025-12-15 19:22:40 +0100

    feature: add invokeAndTransfer to lazyRpc (#319)

--- packages/rpc-registry/src/parts/LazyRpc/LazyRpc.ts
+++ packages/rpc-registry/src/parts/LazyRpc/LazyRpc.ts
@@ -2,5 +2,6 @@ import type { Rpc } from '@lvce-editor/rpc'
 
 export interface LazyRpc {
   readonly invoke: (method: string, ...params: readonly any[]) => Promise<any>
+  readonly invokeAndTransfer: (method: string, ...params: readonly any[]) => Promise<any>
   readonly setFactory: (value: () => Promise<Rpc>) => void
 }
