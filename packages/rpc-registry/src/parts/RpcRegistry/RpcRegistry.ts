653746edca71023fac35853312908e24f8bddca3
Le Vivilet <levivilet@pm.me>
2025-12-15 21:22:11 +0100

    feature: don't throw error when overwriting rpc, since it causes breaking changes for tests where the same rpc type is registered for each test

--- packages/rpc-registry/src/parts/RpcRegistry/RpcRegistry.ts
+++ packages/rpc-registry/src/parts/RpcRegistry/RpcRegistry.ts
@@ -3,9 +3,6 @@ import type { Rpc } from '@lvce-editor/rpc'
 const rpcs: Record<number, Rpc> = Object.create(null)
 
 export const set = (id: number, rpc: Rpc): void => {
-  if (rpcs[id]) {
-    throw new Error(`rpc with id ${id} is already registered`)
-  }
   rpcs[id] = rpc
 }
 
