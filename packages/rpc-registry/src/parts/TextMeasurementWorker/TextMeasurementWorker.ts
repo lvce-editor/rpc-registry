a52c323ec421ffb2faa267ee3ca537c19351c22e
Le Vivilet <levivilet@pm.me>
2026-01-05 18:41:53 +0100

    feature: add measureTextWidth

--- packages/rpc-registry/src/parts/TextMeasurementWorker/TextMeasurementWorker.ts
+++ packages/rpc-registry/src/parts/TextMeasurementWorker/TextMeasurementWorker.ts
@@ -22,3 +22,15 @@ export const measureTextWidth = async (
 ): Promise<number> => {
   return invoke('TextMeasurement.measureTextWidth', text, fontWeight, fontSize, fontFamily, letterSpacing, isMonoSpaceFont, charWidth)
 }
+
+export const measureTextWidths = async (
+  texts: readonly string[],
+  fontWeight: number,
+  fontSize: number,
+  fontFamily: string,
+  letterSpacing: number,
+  isMonoSpaceFont: boolean,
+  charWidth: number,
+): Promise<readonly number[]> => {
+  return invoke('TextMeasurement.measureTextWidths', texts, fontWeight, fontSize, fontFamily, letterSpacing, isMonoSpaceFont, charWidth)
+}
