9636c810ce6d8e2c0cdafec554ad06d9ef786c5f
Le Vivilet <levivilet@pm.me>
2026-01-29 14:32:43 +0100

    feature: add opener functions

--- packages/rpc-registry/src/parts/OpenerWorker/OpenerWorker.ts
+++ packages/rpc-registry/src/parts/OpenerWorker/OpenerWorker.ts
@@ -4,4 +4,18 @@ const rpcId = 4561
 
 export const { dispose, invoke, invokeAndTransfer, set } = RpcFactory.create(rpcId)
 
-// TODO add functions
+export const enableMemoryOpener = async (): Promise<void> => {
+  return invoke('Open.enableMemoryOpener')
+}
+
+export const openExternal = async (url: string): Promise<void> => {
+  return invoke('Open.openExternal', url)
+}
+
+export const openUrl = async (url: string): Promise<void> => {
+  return invoke('Open.openUrl', url)
+}
+
+export const readOpenedUrl = async (): Promise<string> => {
+  return invoke('Open.readOpenedUrl')
+}
