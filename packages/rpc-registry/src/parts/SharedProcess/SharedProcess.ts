52742e3e148254aae467705732b85a1d7dedf552
Le Vivilet <levivilet@pm.me>
2026-02-01 21:52:05 +0100

    fix

--- packages/rpc-registry/src/parts/SharedProcess/SharedProcess.ts
+++ packages/rpc-registry/src/parts/SharedProcess/SharedProcess.ts
@@ -1,4 +1,4 @@
 import { RpcId } from '@lvce-editor/constants'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
-export const { dispose, invoke, invokeAndTransfer, set, registerMockRpc } = RpcFactory.create(RpcId.SharedProcess)
+export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(RpcId.SharedProcess)
