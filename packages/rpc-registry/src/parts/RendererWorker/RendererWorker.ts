83b54ede847bc7ebf63636f1abf5be28c06bd114
Le Vivilet <levivilet@pm.me>
2026-02-01 22:48:19 +0100

    feature: add sendPortToMainAreaWorker

--- packages/rpc-registry/src/parts/RendererWorker/RendererWorker.ts
+++ packages/rpc-registry/src/parts/RendererWorker/RendererWorker.ts
@@ -24,7 +24,6 @@ export const showContextMenu2 = async (uid: number, menuId: any, x: number, y: n
   Assert.number(menuId)
   Assert.number(x)
   Assert.number(y)
-  // @ts-ignore
   await invoke('ContextMenu.show2', uid, menuId, x, y, args)
 }
 
@@ -37,7 +36,6 @@ export const applyBulkReplacement = async (bulkEdits: readonly any[]): Promise<v
 }
 
 export const setColorTheme = async (id: string): Promise<string> => {
-  // @ts-ignore
   return invoke(/* ColorTheme.setColorTheme */ 'ColorTheme.setColorTheme', /* colorThemeId */ id)
 }
 
@@ -75,37 +73,36 @@ export const unregisterWebViewInterceptor = async (id: number): Promise<void> =>
 
 export const sendMessagePortToEditorWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
   const command = 'HandleMessagePort.handleMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker', port, command, rpcId)
 }
 
 export const sendMessagePortToOpenerWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
   const command = 'HandleMessagePort.handleMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToOpenerWorker', port, command, rpcId)
+}
+
+export const sendMessagePortToMainAreaWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
+  const command = 'HandleMessagePort.handleMessagePort'
+  await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToMainAreaWorker', port, command, rpcId)
 }
 
 export const sendMessagePortToErrorWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
   const command = 'Errors.handleMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToErrorWorker', port, command, rpcId)
 }
 
 export const sendMessagePortToMarkdownWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
   const command = 'Markdown.handleMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToMarkdownWorker', port, command, rpcId)
 }
 
 export const sendMessagePortToIconThemeWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
   const command = 'IconTheme.handleMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToIconThemeWorker', port, command, rpcId)
 }
 
 export const sendMessagePortToFileSystemWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
   const command = 'FileSystem.handleMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToFileSystemWorker', port, command, rpcId)
 }
 
@@ -114,7 +111,6 @@ export const readFile = async (uri: string): Promise<string> => {
 }
 
 export const getWebViewSecret = async (key: string): Promise<string> => {
-  // @ts-ignore
   return invoke('WebView.getSecret', key)
 }
 
@@ -135,17 +131,14 @@ export const getColorThemeNames = async (): Promise<readonly string[]> => {
 }
 
 export const disableExtension = async (id: string): Promise<Error | undefined> => {
-  // @ts-ignore
   return invoke('ExtensionManagement.disable', id)
 }
 
 export const enableExtension = async (id: string): Promise<Error | undefined> => {
-  // @ts-ignore
   return invoke('ExtensionManagement.enable', id)
 }
 
 export const handleDebugChange = async (params: any): Promise<void> => {
-  // @ts-ignore
   return invoke('Run And Debug.handleChange', params)
 }
 
@@ -176,7 +169,6 @@ export const sendMessagePortToSearchProcess = async (port: MessagePort): Promise
 }
 
 export const confirm = async (message: string, options?: any): Promise<boolean> => {
-  // @ts-ignore
   const result = await invoke('ConfirmPrompt.prompt', message, options)
   return result
 }
@@ -198,12 +190,10 @@ export const readClipBoardText = async (): Promise<string> => {
 }
 
 export const writeClipBoardImage = async (blob: any): Promise<void> => {
-  // @ts-ignore
   await invoke('ClipBoard.writeImage', /* text */ blob)
 }
 
 export const searchFileMemory = async (uri: string): Promise<void> => {
-  // @ts-ignore
   return invoke('ExtensionHost.searchFileWithMemory', uri)
 }
 
@@ -233,12 +223,10 @@ export const activateByEvent = (event: string, assetDir: string, platform: numbe
 }
 
 export const setAdditionalFocus = (focusKey: number): Promise<void> => {
-  // @ts-ignore
   return invoke('Focus.setAdditionalFocus', focusKey)
 }
 
 export const getActiveEditorId = (): Promise<number> => {
-  // @ts-ignore
   return invoke('GetActiveEditor.getActiveEditorId')
 }
 
@@ -248,25 +236,21 @@ export const getWorkspacePath = (): Promise<string> => {
 
 export const sendMessagePortToRendererProcess = async (port: MessagePort): Promise<void> => {
   const command = 'HandleMessagePort.handleMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToRendererProcess', port, command, RpcId.DebugWorker)
 }
 
 export const sendMessagePortToTextMeasurementWorker = async (port: MessagePort): Promise<void> => {
   const command = 'TextMeasurement.handleMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToTextMeasurementWorker', port, command, 0)
 }
 
 export const sendMessagePortToSourceControlWorker = async (port: MessagePort): Promise<void> => {
   const command = 'SourceControl.handleMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToSourceControlWorker', port, command, 0)
 }
 
 export const sendMessagePortToSharedProcess = async (port: MessagePort): Promise<void> => {
   const command = 'HandleElectronMessagePort.handleElectronMessagePort'
-  // @ts-ignore
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToSharedProcess', port, command, 0)
 }
 
@@ -294,7 +278,6 @@ export const getAllExtensions = async (): Promise<readonly any[]> => {
 }
 
 export const rerenderEditor = async (key: string): Promise<void> => {
-  // @ts-ignore
   return invoke('Editor.rerender', key)
 }
 
@@ -307,12 +290,7 @@ export const openUri = async (uri: string, focus?: boolean, options?: any): Prom
 }
 
 export const sendMessagePortToSyntaxHighlightingWorker = async (port: MessagePort): Promise<void> => {
-  await invokeAndTransfer(
-    // @ts-ignore
-    'SendMessagePortToSyntaxHighlightingWorker.sendMessagePortToSyntaxHighlightingWorker',
-    port,
-    'HandleMessagePort.handleMessagePort2',
-  )
+  await invokeAndTransfer('SendMessagePortToSyntaxHighlightingWorker.sendMessagePortToSyntaxHighlightingWorker', port, 'HandleMessagePort.handleMessagePort2')
 }
 
 export const handleDebugScriptParsed = async (script: any): Promise<void> => {
@@ -324,7 +302,6 @@ export const getWindowId = async (): Promise<number> => {
 }
 
 export const getBlob = async (uri: string): Promise<Blob> => {
-  // @ts-ignore
   return invoke('FileSystem.getBlob', uri)
 }
 
@@ -333,32 +310,26 @@ export const getExtensionCommands = async (): Promise<readonly any[]> => {
 }
 
 export const showErrorDialog = async (errorInfo: any): Promise<void> => {
-  // @ts-ignore
   await invoke('ErrorHandling.showErrorDialog', errorInfo)
 }
 
 export const getFolderSize = async (uri: string): Promise<number> => {
-  // @ts-ignore
   return await invoke('FileSystem.getFolderSize', uri)
 }
 
 export const getExtension = async (id: string): Promise<any> => {
-  // @ts-ignore
   return invoke('ExtensionManagement.getExtension', id)
 }
 
 export const getMarkdownDom = async (html: string): Promise<any> => {
-  // @ts-ignore
   return invoke('Markdown.getVirtualDom', html)
 }
 
 export const renderMarkdown = async (markdown: string, options: any): Promise<any> => {
-  // @ts-ignore
   return invoke('Markdown.renderMarkdown', markdown, options)
 }
 
 export const openNativeFolder = async (uri: string): Promise<void> => {
-  // @ts-ignore
   await invoke('OpenNativeFolder.openNativeFolder', uri)
 }
 
@@ -367,62 +338,50 @@ export const uninstallExtension = async (id: string): Promise<void> => {
 }
 
 export const installExtension = async (id: string): Promise<void> => {
-  // @ts-ignore
   return invoke('ExtensionManagement.install', id)
 }
 
 export const minimizeWindow = async (): Promise<void> => {
-  // @ts-ignore
   return invoke('ElectronWindow.minimize')
 }
 
 export const unmaximizeWindow = async (): Promise<void> => {
-  // @ts-ignore
   return invoke('ElectronWindow.unmaximize')
 }
 
 export const maximizeWindow = async (): Promise<void> => {
-  // @ts-ignore
   return invoke('ElectronWindow.maximize')
 }
 
 export const closeWindow = async (): Promise<void> => {
-  // @ts-ignore
   return invoke('ElectronWindow.close')
 }
 
 export const openExtensionSearch = async (): Promise<void> => {
-  // @ts-ignore
   return invoke('SideBar.openViewlet', 'Extensions')
 }
 
 export const setExtensionsSearchValue = async (searchValue: string): Promise<void> => {
-  // @ts-ignore
   return invoke('Extensions.handleInput', searchValue, InputSource.Script)
 }
 
 export const openExternal = async (uri: string): Promise<void> => {
-  // @ts-ignore
   await invoke('Open.openExternal', uri)
 }
 
 export const openUrl = async (uri: string): Promise<void> => {
-  // @ts-ignore
   await invoke('Open.openUrl', uri)
 }
 
 export const getAllPreferences = async (): Promise<any> => {
-  // @ts-ignore
   return invoke('Preferences.getAll')
 }
 
 export const showSaveFilePicker = async (): Promise<string> => {
-  // @ts-ignore
   return invoke('FilePicker.showSaveFilePicker')
 }
 
 export const getLogsDir = async (): Promise<string> => {
-  // @ts-ignore
   return invoke('PlatformPaths.getLogsDir')
 }
 
