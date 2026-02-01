528f453e7aca8e68add4b2b6f83c24bc0c4a7bdf
Le Vivilet <levivilet@pm.me>
2026-02-01 15:25:52 +0100

    feature: add registerMockRpc to MainProcess and SharedProcess

--- packages/rpc-registry/src/parts/MainProcess/MainProcess.ts
+++ packages/rpc-registry/src/parts/MainProcess/MainProcess.ts
@@ -1,4 +1,4 @@
 import { RpcId } from '@lvce-editor/constants'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
-export const { dispose, invoke, invokeAndTransfer, set } = RpcFactory.create(RpcId.MainProcess)
+export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(RpcId.MainProcess)
