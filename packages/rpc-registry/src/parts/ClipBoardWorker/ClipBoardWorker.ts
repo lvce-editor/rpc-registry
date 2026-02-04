a1d6881de7636f8e3908b5f1d74fbb2fb3c0ae5b
Le Vivilet <levivilet@pm.me>
2026-02-04 02:18:35 +0100

    feature: add registerMockRpc to ClipBoardWorker exports

--- packages/rpc-registry/src/parts/ClipBoardWorker/ClipBoardWorker.ts
+++ packages/rpc-registry/src/parts/ClipBoardWorker/ClipBoardWorker.ts
@@ -1,24 +1,20 @@
 import { RpcId } from '@lvce-editor/constants'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
-export const { dispose, invoke, invokeAndTransfer, set } = RpcFactory.create(RpcId.ClipBoardWorker)
+export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(RpcId.ClipBoardWorker)
 
 export const writeText = async (text: string): Promise<void> => {
-  // @ts-ignore
   return invoke('ClipBoard.writeText', text)
 }
 
 export const readText = async (): Promise<string> => {
-  // @ts-ignore
   return invoke('ClipBoard.readText')
 }
 
 export const writeImage = async (image: any): Promise<void> => {
-  // @ts-ignore
   return invoke('ClipBoard.writeImage', image)
 }
 
 export const readNativeFiles = async (): Promise<readonly any[]> => {
-  // @ts-ignore
   return invoke('ClipBoard.readNativeFiles')
 }
