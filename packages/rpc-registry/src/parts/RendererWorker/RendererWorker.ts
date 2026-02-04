6dd3f35323f3035fcbd530491d7ec8051d679d61
Le Vivilet <levivilet@pm.me>
2026-02-04 02:15:58 +0100

    feature: add sendMessagePortToClipBoardWorker function to RendererWorker

--- packages/rpc-registry/src/parts/RendererWorker/RendererWorker.ts
+++ packages/rpc-registry/src/parts/RendererWorker/RendererWorker.ts
@@ -76,6 +76,11 @@ export const sendMessagePortToEditorWorker = async (port: MessagePort, rpcId: nu
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToEditorWorker', port, command, rpcId)
 }
 
+export const sendMessagePortToClipBoardWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
+  const command = 'ClipBoard.handleMessagePort'
+  await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToClipBoardWorker', port, command, rpcId)
+}
+
 export const sendMessagePortToOpenerWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
   const command = 'HandleMessagePort.handleMessagePort'
   await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToOpenerWorker', port, command, rpcId)
