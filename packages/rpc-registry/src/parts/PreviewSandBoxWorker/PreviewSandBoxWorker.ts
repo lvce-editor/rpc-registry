7334cbbe1fefa633dad2faf3b1dcb7d71f9f374b
Le Vivilet <levivilet@pm.me>
2026-02-07 23:17:39 +0100

    feature: add handleInput, handleKeyDown, handleKeyUp, and initialize functions to PreviewSandBoxWorker (#385)

--- packages/rpc-registry/src/parts/PreviewSandBoxWorker/PreviewSandBoxWorker.ts
+++ packages/rpc-registry/src/parts/PreviewSandBoxWorker/PreviewSandBoxWorker.ts
@@ -5,3 +5,19 @@ export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcF
 export const terminate = async (): Promise<void> => {
   return invoke('SandBox.terminate')
 }
+
+export const handleInput = async (uid: number, hdId: string, value: string): Promise<void> => {
+  return invoke('SandBox.handleInput', uid, hdId, value)
+}
+
+export const handleKeyDown = async (uid: number, hdId: string, key: string): Promise<void> => {
+  return invoke('SandBox.handleKeyDown', uid, hdId, key)
+}
+
+export const handleKeyUp = async (uid: number, hdId: string, key: string): Promise<void> => {
+  return invoke('SandBox.handleKeyUp', uid, hdId, key)
+}
+
+export const initialize = async (uid: number, content: string, scripts: readonly string[]): Promise<void> => {
+  return invoke('SandBox.initialize', uid, content, scripts)
+}
