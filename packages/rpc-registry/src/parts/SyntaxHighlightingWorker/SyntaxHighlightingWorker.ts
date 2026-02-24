ed4b7742d978a3f989d1b73d39e6da9db1ade01a
Le Vivilet <levivilet@pm.me>
2026-02-24 15:09:30 +0100

    feature: add tokenizeIncremental function to SyntaxHighlightingWorker (#397)

--- packages/rpc-registry/src/parts/SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts
+++ packages/rpc-registry/src/parts/SyntaxHighlightingWorker/SyntaxHighlightingWorker.ts
@@ -2,3 +2,7 @@ import { RpcId } from '@lvce-editor/constants'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
 export const { dispose, invoke, invokeAndTransfer, set } = RpcFactory.create(RpcId.MarkdownWorker)
+
+export const tokenizeIncremental = (uid: number, languageId: string, oldLine: string, newLine: string, rowIndex: number, minLineY: number): Promise<readonly any[]> => {
+  return invoke('TokenizeIncremental.tokenizeIncremental', uid, languageId, oldLine, newLine, rowIndex, minLineY)
+}
