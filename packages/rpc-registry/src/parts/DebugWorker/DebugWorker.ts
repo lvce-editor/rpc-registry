4b8055da0dac4073f24b181ae0c10ff52071aec4
Le Vivilet <levivilet@pm.me>
2026-02-24 15:06:58 +0100

    feature: add DebugHighlight interface and getDebugHighlights function

--- packages/rpc-registry/src/parts/DebugWorker/DebugWorker.ts
+++ packages/rpc-registry/src/parts/DebugWorker/DebugWorker.ts
@@ -2,3 +2,13 @@ import { RpcId } from '@lvce-editor/constants'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
 export const { dispose, invoke, invokeAndTransfer, set } = RpcFactory.create(RpcId.DebugWorker)
+
+export interface DebugHighlight {
+  readonly columnIndex: number
+  readonly rowIndex: number
+  readonly uri: string
+}
+
+export const getDebugHighlights = async (debugId: number): Promise<readonly DebugHighlight[]> => {
+  return invoke('RunAndDebug.getHighlight', debugId)
+}
