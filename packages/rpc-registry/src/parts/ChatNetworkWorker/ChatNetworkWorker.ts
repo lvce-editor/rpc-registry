59cb1dbc6827209d152654024ba08362d414bd53
Le Vivilet <levivilet@pm.me>
2026-03-11 08:07:36 +0100

    fix: replace hardcoded RPC IDs with constants in ChatNetworkWorker and ChatStorageWorker (#416)

--- packages/rpc-registry/src/parts/ChatNetworkWorker/ChatNetworkWorker.ts
+++ packages/rpc-registry/src/parts/ChatNetworkWorker/ChatNetworkWorker.ts
@@ -1,6 +1,7 @@
+import { RpcId } from '@lvce-editor/constants'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
-export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(6002)
+export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(RpcId.ChatNetworkWorker)
 
 export const makeApiRequest = async (options: any): Promise<any> => {
   return invoke('ChatNetwork.makeApiRequest', options)
