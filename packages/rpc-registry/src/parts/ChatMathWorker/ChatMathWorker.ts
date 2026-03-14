9b81cf38ff9ac0b1d4cccb9983d232896fe1e3f3
Le Vivilet <levivilet@pm.me>
2026-03-14 08:08:29 +0100

    feature: add MessageMathBlockNode and MessageMathInlineNode interfaces with corresponding DOM retrieval functions

--- packages/rpc-registry/src/parts/ChatMathWorker/ChatMathWorker.ts
+++ packages/rpc-registry/src/parts/ChatMathWorker/ChatMathWorker.ts
@@ -2,10 +2,21 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
 export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(6007)
 
-export const create = async (options: any): Promise<any> => {
-  return invoke('ChatDebug.create', options)
+export interface MessageMathBlockNode {
+  readonly text: string
+  readonly type: 'math-block'
 }
 
-export const loadContent = async (options: any): Promise<any> => {
-  return invoke('ChatDebug.loadContent', options)
+export interface MessageMathInlineNode {
+  readonly displayMode: boolean
+  readonly text: string
+  readonly type: 'math-inline'
+}
+
+export const getMathBlockDom = async (node: MessageMathBlockNode): Promise<any> => {
+  return invoke('ChatMath.getMathBlockDom', node)
+}
+
+export const getMathInlineDom = async (node: MessageMathInlineNode): Promise<any> => {
+  return invoke('ChatMath.getMathInlineDom', node)
 }
