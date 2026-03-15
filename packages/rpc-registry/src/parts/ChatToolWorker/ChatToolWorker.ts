fd702dd367016c2300c3b2a68c5c93124662ec3f
Le Vivilet <levivilet@pm.me>
2026-03-15 23:01:11 +0100

    feature: refactor ChatToolWorker to define ChatTool and ExecuteToolOptions types (#429)

--- packages/rpc-registry/src/parts/ChatToolWorker/ChatToolWorker.ts
+++ packages/rpc-registry/src/parts/ChatToolWorker/ChatToolWorker.ts
@@ -3,10 +3,29 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
 export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(RpcId.ChatToolWorker)
 
-export const save = async (options: any): Promise<any> => {
-  return invoke('ChatStorage.getTools', options)
+export type ChatTool = {
+  readonly type: 'function'
+  readonly function: {
+    readonly name: string
+    readonly description: string
+    readonly parameters: {
+      readonly type: 'object'
+      readonly properties: Record<string, unknown>
+      readonly required?: readonly string[]
+      readonly additionalProperties: boolean
+    }
+  }
 }
 
-export const get = async (options: any): Promise<any> => {
-  return invoke('ChatStorage.executeTool', options)
+export type ExecuteToolOptions = {
+  readonly assetDir: string
+  readonly platform: number
+}
+
+export const execute = async (name: string, rawArguments: unknown, options: ExecuteToolOptions): Promise<any> => {
+  return invoke('ChatTool.execute', name, rawArguments, options)
+}
+
+export const getTools = async (): Promise<readonly ChatTool[]> => {
+  return invoke('ChatTool.getTools')
 }
