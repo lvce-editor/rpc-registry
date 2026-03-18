56ec858584715744138cbda035913bf28466da9a
Le Vivilet <levivilet@pm.me>
2026-03-18 15:54:24 +0100

    feature: add TextSearchWorker module with API request functions (#432)

---
+++ packages/rpc-registry/src/parts/TextSearchWorker/TextSearchWorker.ts
@@ -0,0 +1,12 @@
+import { RpcId } from '@lvce-editor/constants'
+import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
+
+export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(RpcId.TextSearchWorker)
+
+export const makeApiRequest = async (options: any): Promise<any> => {
+  return invoke('ChatNetwork.makeApiRequest', options)
+}
+
+export const makeStreamingApiRequest = async (options: any): Promise<any> => {
+  return invoke('ChatNetwork.makeStreamingApiRequest', options)
+}
