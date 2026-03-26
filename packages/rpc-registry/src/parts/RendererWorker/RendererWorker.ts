62768be729924fca4a261bb641567203cae57554
Le Vivilet <levivilet@pm.me>
2026-03-26 14:35:02 +0100

    typo

--- packages/rpc-registry/src/parts/RendererWorker/RendererWorker.ts
+++ packages/rpc-registry/src/parts/RendererWorker/RendererWorker.ts
@@ -102,7 +102,7 @@ export const sendMessagePortToChatCoordinatorWorker = async (port: MessagePort, 
 
 export const sendMessagePortToChatMessageParsingWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
   const command = 'HandleMessagePort.handleMessagePort'
-  await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToChatMessageParsingWorkerWorker', port, command, rpcId)
+  await invokeAndTransfer('SendMessagePortToExtensionHostWorker.sendMessagePortToChatMessageParsingWorker', port, command, rpcId)
 }
 
 export const sendMessagePortToMainAreaWorker = async (port: MessagePort, rpcId: number): Promise<void> => {
