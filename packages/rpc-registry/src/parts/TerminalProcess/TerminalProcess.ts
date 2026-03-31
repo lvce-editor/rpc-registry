1e37646253f3d4d86449084219fd678deb1d82c8
Le Vivilet <levivilet@pm.me>
2026-03-31 22:29:20 +0200

    feature: add TerminalProcess for RPC handling

---
+++ packages/rpc-registry/src/parts/TerminalProcess/TerminalProcess.ts
@@ -0,0 +1,4 @@
+import { RpcId } from '@lvce-editor/constants'
+import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
+
+export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(RpcId.TerminalProcess)
