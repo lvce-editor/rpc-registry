945d4755a8189f3872bcb4d2c50c9c580ec38e1b
Le Vivilet <levivilet@pm.me>
2026-04-02 18:28:50 +0200

    feature: rename makeApiRequest and makeStreamingApiRequest to login and logout for clarity

--- packages/rpc-registry/src/parts/AuthWorker/AuthWorker.ts
+++ packages/rpc-registry/src/parts/AuthWorker/AuthWorker.ts
@@ -2,10 +2,10 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
 export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(6010)
 
-export const makeApiRequest = async (options: any): Promise<any> => {
-  return invoke('ChatNetwork.makeApiRequest', options)
+export const login = async (options: any): Promise<any> => {
+  return invoke('Auth.login', options)
 }
 
-export const makeStreamingApiRequest = async (options: any): Promise<any> => {
-  return invoke('ChatNetwork.makeStreamingApiRequest', options)
+export const logout = async (options: any): Promise<any> => {
+  return invoke('Auth.logout', options)
 }
