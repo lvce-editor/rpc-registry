af5b30110951aeaeda2b9c948933266a5994bc2e
Le Vivilet <levivilet@pm.me>
2026-04-16 14:54:01 +0200

    feature: refactor BlobWorker to consolidate clipboard operations into Blob methods (#456)

--- packages/rpc-registry/src/parts/BlobWorker/BlobWorker.ts
+++ packages/rpc-registry/src/parts/BlobWorker/BlobWorker.ts
@@ -2,18 +2,22 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
 export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(19_000)
 
-export const writeText = async (text: string): Promise<void> => {
-  return invoke('ClipBoard.writeText', text)
+export const base64StringToBlob = async (base64String: string, type: string): Promise<Blob> => {
+  return invoke('Blob.base64StringToBlob', base64String, type)
 }
 
-export const readText = async (): Promise<string> => {
-  return invoke('ClipBoard.readText')
+export const binaryStringToBlob = async (binaryString: string, type: string): Promise<Blob> => {
+  return invoke('Blob.binaryStringToBlob', binaryString, type)
 }
 
-export const writeImage = async (image: any): Promise<void> => {
-  return invoke('ClipBoard.writeImage', image)
+export const blobToBinaryString = async (blob: Blob): Promise<string> => {
+  return invoke('Blob.blobToBinaryString', blob)
 }
 
-export const readNativeFiles = async (): Promise<readonly any[]> => {
-  return invoke('ClipBoard.readNativeFiles')
+export const handleMessagePort = async (port: MessagePort): Promise<void> => {
+  return invokeAndTransfer('Blob.handleMessagePort', port)
+}
+
+export const terminate = async (): Promise<void> => {
+  return invoke('Blob.terminate')
 }
