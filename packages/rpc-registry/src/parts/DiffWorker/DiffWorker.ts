58a600d2d6c976c164a19eaaf74d3c7d3cca225c
Le Vivilet <levivilet@pm.me>
2026-04-18 11:42:09 +0200

    fix

--- packages/rpc-registry/src/parts/DiffWorker/DiffWorker.ts
+++ packages/rpc-registry/src/parts/DiffWorker/DiffWorker.ts
@@ -2,7 +2,7 @@ import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
 export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(218_933)
 
-export const execute = async (name: string, rawArguments: unknown, options: ExecuteToolOptions): Promise<any> => {
+export const execute = async (name: string, rawArguments: unknown, options: any): Promise<any> => {
   return invoke('Diff.execute', name, rawArguments, options)
 }
 
