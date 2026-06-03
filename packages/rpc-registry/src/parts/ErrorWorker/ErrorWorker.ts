9df285fc5f1ebb98757eb0f57044fcff05b25a0b
Le Vivilet <levivilet@pm.me>
2026-06-03 16:28:20 +0200

    feature: add PrettyError interface and prepare function for error handling

--- packages/rpc-registry/src/parts/ErrorWorker/ErrorWorker.ts
+++ packages/rpc-registry/src/parts/ErrorWorker/ErrorWorker.ts
@@ -2,3 +2,13 @@ import { RpcId } from '@lvce-editor/constants'
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
 export const { dispose, invoke, invokeAndTransfer, set } = RpcFactory.create(RpcId.ErrorWorker)
+
+export interface PrettyError {
+  readonly codeFrame: string | undefined
+  readonly message: string | undefined
+  readonly stack: string | undefined
+}
+
+export const prepare = async (error: any): Promise<PrettyError> => {
+  return invoke('Errors.prepare', error)
+}
