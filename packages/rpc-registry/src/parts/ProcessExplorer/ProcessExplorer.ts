6c6c59c66472d8d63657a99630c3fda334efda09
Le Vivilet <levivilet@pm.me>
2026-07-04 22:56:17 +0200

    feature: add getMainProcessId and listProcessesWithMemoryUsage functions

--- packages/rpc-registry/src/parts/ProcessExplorer/ProcessExplorer.ts
+++ packages/rpc-registry/src/parts/ProcessExplorer/ProcessExplorer.ts
@@ -1,3 +1,17 @@
 import * as RpcFactory from '../RpcFactory/RpcFactory.ts'
 
 export const { dispose, invoke, invokeAndTransfer, registerMockRpc, set } = RpcFactory.create(10_001)
+
+export interface GetMainProcessIdOptions {
+  readonly includeElectronData: boolean
+}
+
+export const getMainProcessId = async (options: GetMainProcessIdOptions): Promise<number> => {
+  const result = await invoke('ProcessId.getMainProcessId', options)
+  return result
+}
+
+export const listProcessesWithMemoryUsage = async (rootPid: number, includeElectronData: boolean): Promise<readonly any[]> => {
+  // @ts-ignore
+  return invoke('ListProcessesWithMemoryUsage.listProcessesWithMemoryUsage', rootPid, includeElectronData)
+}
