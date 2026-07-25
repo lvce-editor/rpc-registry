2faf8cacf03bf580953042d825491b3330ec2158
Le Vivilet <levivilet@pm.me>
2026-07-25 13:30:34 +0000

    feature: add file permission process rpc

--- packages/rpc-registry/test/Main.test.ts
+++ packages/rpc-registry/test/Main.test.ts
@@ -2,5 +2,24 @@ import { expect, test } from '@jest/globals'
 import * as Index from '../src/parts/Main/Main.ts'
 
 test('index', () => {
+  expect(typeof Index.FilePermissionProcess.invoke).toBe('function')
   expect(typeof Index.get).toBe('function')
+})
+
+test('sendMessagePortToFilePermissionProcess', async () => {
+  using mockRendererRpc = Index.RendererWorker.registerMockRpc({
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToSharedProcess'() {},
+  })
+  const port = {} as MessagePort
+
+  await Index.RendererWorker.sendMessagePortToFilePermissionProcess(port, Index.RpcId.DialogWorker)
+
+  expect(mockRendererRpc.invocations).toEqual([
+    [
+      'SendMessagePortToExtensionHostWorker.sendMessagePortToSharedProcess',
+      port,
+      'HandleMessagePortForFilePermissionProcess.handleMessagePortForFilePermissionProcess',
+      Index.RpcId.DialogWorker,
+    ],
+  ])
 })
