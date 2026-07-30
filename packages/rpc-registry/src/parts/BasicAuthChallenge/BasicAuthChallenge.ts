2b7ff49a0b334f4da26ba60c3fff00af7468e0ac
Le Vivilet <levivilet@pm.me>
2026-07-30 13:55:04 -0700

    feature: add knip (#516)

--- packages/rpc-registry/src/parts/BasicAuthChallenge/BasicAuthChallenge.ts
+++
@@ -1,9 +0,0 @@
-export interface BasicAuthChallenge {
-  readonly host: string
-  readonly isProxy: boolean
-  readonly port: number
-  readonly realm: string
-  readonly requestId: string
-  readonly scheme: string
-  readonly url: string
-}
